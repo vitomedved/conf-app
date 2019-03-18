6c26be40908ab0d7348254d0c5b1c974e709d5ac
tonto98 <rasanko@gmail.com>
2019-03-18 16:23:55 +0100

    added CUsers class :)

---
+++ app/src/main/java/com/example/confapp/CUsers.kt
@@ -0,0 +1,15 @@
+package com.example.confapp
+
+class CUsers(
+    val id: Int = -1,
+    val avatar_id: String = "",
+    val mail: String = "",
+    val name: String = "",
+    val password: String = "",
+    val subscribedEvents: List<Int> = emptyList()
+) {
+
+
+
+
+}
