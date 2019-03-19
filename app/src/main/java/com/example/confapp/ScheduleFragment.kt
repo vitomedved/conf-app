4051e92bdf75633f1c3875512c7bcddea5004cb6
vmedved <vito.medved12@gmail.com>
2019-03-19 23:49:21 +0100

    Add recycler view to schedule fragment Add Schedule fragment adapter class Add colors Change no_connection fragment

--- app/src/main/java/com/example/confapp/ScheduleFragment.kt
+++ app/src/main/java/com/example/confapp/ScheduleFragment.kt
@@ -1,22 +1,86 @@
 package com.example.confapp
 
 
+import android.content.Context
+import android.net.ConnectivityManager
+import android.net.NetworkInfo
 import android.os.Bundle
 import android.support.v4.app.Fragment
+import android.support.v7.widget.LinearLayoutManager
+import android.support.v7.widget.RecyclerView
+import android.util.Log
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.Toast
+import com.firebase.client.DataSnapshot
+import com.firebase.client.Firebase
+import com.firebase.client.FirebaseError
+import com.firebase.client.ValueEventListener
 
 
 
+@Suppress("DEPRECATION")
 class ScheduleFragment : Fragment() {
 
-    override fun onCreateView(
-        inflater: LayoutInflater, container: ViewGroup?,
-        savedInstanceState: Bundle?
-    ): View? {
-        // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_schedule, container, false)
+    private lateinit var firebaseRef: Firebase
+    private val events: MutableList<CEvent> = mutableListOf()
+
+    private lateinit var retView: View
+    private lateinit var recyclerView: RecyclerView
+
+    override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?): View? {
+
+        //events.clear()
+
+        val connectivityManager = context?.getSystemService(Context.CONNECTIVITY_SERVICE) as ConnectivityManager
+        val activeNetwork: NetworkInfo? = connectivityManager.activeNetworkInfo
+        val isConnected: Boolean = activeNetwork?.isConnectedOrConnecting == true
+
+        if(!isConnected)
+        {
+            Toast.makeText(context, "Please connect to the internet", Toast.LENGTH_LONG).show()
+            return inflater.inflate(R.layout.fragment_no_connection, container, false)
+        }
+
+        retView = inflater.inflate(R.layout.fragment_schedule, container, false)
+
+        recyclerView = retView.findViewById(R.id.recyclerView_schedule)
+        recyclerView.layoutManager = LinearLayoutManager(activity)//LinearLayoutManager(this.context, LinearLayout.VERTICAL, false)
+
+        Firebase.setAndroidContext(this.context)
+
+        firebaseRef = Firebase("https://conf-app-14914.firebaseio.com")
+
+        firebaseRef.child("Data/event").addValueEventListener(object: ValueEventListener {
+            override fun onCancelled(p0: FirebaseError?) {
+                Log.d("FIREBASE", "Events from database are not loaded.")
+                return
+            }
+
+            override fun onDataChange(dataSnapshot: DataSnapshot) {
+                events.clear()
+                for(eventSnapshot in dataSnapshot.children){
+                    val event: CEvent = eventSnapshot.getValue(CEvent::class.java)
+                    events.add(event)
+                }
+
+                Log.d("FIREBASE", "Events loaded from database, adding adapter. Total events: " + events.size)
+
+                val adapter = CScheduleRecyclerAdapter(events)
+                recyclerView.adapter = adapter
+            }
+        })
+
+        //val timestamp: Long = System.currentTimeMillis()
+        return retView
+    }
+
+    override fun setUserVisibleHint(isVisibleToUser: Boolean) {
+        val ft = fragmentManager!!.beginTransaction()
+        ft.detach(this).attach(this).commit()
+
+        //events.clear()
     }
 
 
--- app/src/main/java/com/example/confapp/ViewPagerAdapter.kt
+++ app/src/main/java/com/example/confapp/ViewPagerAdapter.kt
@@ -3,17 +3,21 @@ package com.example.confapp
 import android.support.v4.app.Fragment
 import android.support.v4.app.FragmentManager
 import android.support.v4.app.FragmentPagerAdapter
+import android.util.Log
+import android.view.View
 
 class ViewPagerAdapter(fm: FragmentManager) : FragmentPagerAdapter(fm) {
 
     override fun getItem(position: Int): Fragment {
         return when (position) {
             0 -> {
+                Log.d("TABLAYOUT", "Clicked on 0")
                 ScheduleFragment()
             }
             //1 -> BlankFragment()
             else -> {
-                return ScheduleFragment()
+                Log.d("TABLAYOUT", "Clicked on 1")
+                ScheduleFragment()
             }
         }
     }
