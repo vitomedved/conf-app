7b1a8a6cd0156e965cd5f99367b2ca853e9cf3be
tonto98 <rasanko@gmail.com>
2019-03-20 19:32:47 +0100

    Added register activity

---
+++ app/src/main/java/com/example/confapp/RegisterActivity.kt
@@ -0,0 +1,15 @@
+package com.example.confapp
+
+import android.support.v7.app.AppCompatActivity
+import android.os.Bundle
+import android.widget.TextView
+import kotlinx.android.synthetic.main.activity_login.*
+
+class RegisterActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_register)
+
+    }
+}
