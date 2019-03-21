c6f02c4e41717d1695426a4f9c181b675f6bf181
tonto98 <rasanko@gmail.com>
2019-03-21 12:15:52 +0100

    Added Registration/Login activity navigation

--- app/src/main/java/com/example/confapp/RegistrationActivity.kt
+++ app/src/main/java/com/example/confapp/RegistrationActivity.kt
@@ -1,12 +1,38 @@
 package com.example.confapp
 
+import android.content.Intent
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
+import android.widget.Button
+import android.widget.EditText
+import android.widget.TextView
 
 class RegistrationActivity : AppCompatActivity() {
+    lateinit var username: EditText
+    lateinit var email: EditText
+    lateinit var password: EditText
+    lateinit var registerBtn: Button
+    lateinit var alreadyHaveAcc: TextView
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_registration)
+
+        username = findViewById(R.id.editText_username_registration)
+        email = findViewById(R.id.editText_email_registration)
+        password = findViewById(R.id.editText_password_registration)
+        registerBtn = findViewById(R.id.button_register_registration)
+        alreadyHaveAcc = findViewById(R.id.textView_alreadyHaveAcc_registration)
+
+
+        registerBtn.setOnClickListener {
+
+        }
+
+        alreadyHaveAcc.setOnClickListener {
+            val intent = Intent(this, LoginActivity::class.java)
+            startActivity(intent)
+        }
     }
 }
