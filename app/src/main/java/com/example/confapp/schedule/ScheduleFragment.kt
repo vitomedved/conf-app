37ff0e8e6e34962b9bd5cb8813334ce53f895aa1
vmedved <vito.medved12@gmail.com>
2019-04-06 19:18:20 +0200

    Changes to ScheduleFragment due to wrong commit.

--- app/src/main/java/com/example/confapp/schedule/ScheduleFragment.kt
+++ app/src/main/java/com/example/confapp/schedule/ScheduleFragment.kt
@@ -10,15 +10,11 @@ import android.support.v4.app.Fragment
 import android.support.v4.widget.SwipeRefreshLayout
 import android.support.v7.widget.LinearLayoutManager
 import android.support.v7.widget.RecyclerView
-import android.util.Log
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import android.widget.*
-import com.firebase.client.Firebase
-import com.example.confapp.model.CEvent
 import com.example.confapp.R
-import java.text.SimpleDateFormat
 import java.util.*
 
 
