13151c7a88c0bb1c00d0a9c1f71512b2b19682e7
MateaIgnatoski <matea.ignatoski@gmail.com>
2019-04-30 22:09:07 +0200

    exhibitor list item edited

--- app/src/main/java/com/example/confapp/exhibitors/ExhibitorsRecyclerAdapter.kt
+++ app/src/main/java/com/example/confapp/exhibitors/ExhibitorsRecyclerAdapter.kt
@@ -4,15 +4,17 @@ import android.support.v7.widget.RecyclerView
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.ImageView
 import android.widget.TextView
 import com.example.confapp.R
 import com.example.confapp.model.CExhibitor
+import com.squareup.picasso.Picasso
 
 class ExhibitorsRecyclerAdapter(val exhibitorsList: MutableList<CExhibitor>): RecyclerView.Adapter<ExhibitorsRecyclerAdapter.ViewHolder>() {
 
     class ViewHolder(itemView: View): RecyclerView.ViewHolder(itemView){
         val exhibitorCompany = itemView.findViewById(R.id.textView_exhibitorCompany) as TextView
-
+        val exhibitorLogo = itemView.findViewById(R.id.imageView_exhibitorLogo) as ImageView
     }
 
 
@@ -29,8 +31,11 @@ class ExhibitorsRecyclerAdapter(val exhibitorsList: MutableList<CExhibitor>): Re
         val exhibitor: CExhibitor = exhibitorsList[p1]
 
         p0.exhibitorCompany.text = exhibitor.company
+        Picasso.get().load(exhibitor.logo_url).into(p0.exhibitorLogo)
+
     }
 
 
 
 }
+
--- app/src/main/java/com/example/confapp/model/CExhibitor.kt
+++ app/src/main/java/com/example/confapp/model/CExhibitor.kt
@@ -3,6 +3,7 @@ package com.example.confapp.model
 class CExhibitor (
     var company: String = "",
     val id: String = "",
+    val logo_url: String = "",
     var presenters: List<String> = emptyList()
 ){
 }
