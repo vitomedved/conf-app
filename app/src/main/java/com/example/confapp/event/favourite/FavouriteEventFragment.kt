462ca0db1892dc7c465924bb86bbb7e00749fe51
vmedved <vito.medved12@gmail.com>
2019-06-01 16:07:55 +0200

    Finished implementing favouriteEventFragment with adapter

--- app/src/main/java/com/example/confapp/event/favourite/FavouriteEventFragment.kt
+++ app/src/main/java/com/example/confapp/event/favourite/FavouriteEventFragment.kt
@@ -6,26 +6,37 @@ import android.arch.lifecycle.ViewModelProvider
 import android.arch.lifecycle.ViewModelProviders
 import android.os.Bundle
 import android.support.v4.app.Fragment
+import android.support.v7.widget.LinearLayoutManager
+import android.util.Log
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-import android.widget.ProgressBar
 import com.example.confapp.R
+import com.example.confapp.event.favourite.recycler.FavouriteEventAdapter
 import kotlinx.android.synthetic.main.fragment_favourite_event.view.*
 
 class FavouriteEventFragment : Fragment() {
     private lateinit var retView: View
     private lateinit var viewModel: FavouriteEventViewModel
 
-    private lateinit var progressBar: ProgressBar
+    private val adapter = FavouriteEventAdapter()
 
     override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?): View? {
         retView = inflater.inflate(R.layout.fragment_favourite_event, container, false)
 
         viewModel = ViewModelProviders.of(this).get(FavouriteEventViewModel::class.java)
 
+        retView.fragment_favourite_event_recycler.layoutManager = LinearLayoutManager(activity)
+
+        viewModel.events.observe(this, Observer { newEvents ->
+            // TODO: its bad to do this like this. Should create method in adapter setData(events) which will then notify for change of data
+            adapter.m_events = newEvents!!
+            retView.fragment_favourite_event_recycler.adapter = adapter
+            retView.fragment_favourite_event_progress_bar.visibility = View.INVISIBLE
+        })
+
         viewModel.isProgressBarLoading.observe(this, Observer { isLoading ->
-            when(isLoading){
+            when (isLoading) {
                 true -> {
                     retView.fragment_favourite_event_progress_bar.visibility = View.VISIBLE
                 }
@@ -37,5 +48,10 @@ class FavouriteEventFragment : Fragment() {
         })
 
         return retView
+    }
+
+    override fun onResume() {
+        super.onResume()
+        viewModel.initViewModel()
     }
 }
