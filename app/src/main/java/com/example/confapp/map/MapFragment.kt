49c13e1aba78355593e5ed97fa0e6eb319682362
tonto98 <rasanko@gmail.com>
2019-06-06 17:38:10 +0200

    - added Map image with Image Buttons over halls

--- app/src/main/java/com/example/confapp/map/MapFragment.kt
+++ app/src/main/java/com/example/confapp/map/MapFragment.kt
@@ -5,15 +5,30 @@ import android.support.v4.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.ImageButton
+import android.widget.ImageView
+import android.widget.TextView
 import com.example.confapp.R
 
 class MapFragment: Fragment() {
 
     private lateinit var retView: View
+    private lateinit var hallBtn1: ImageView
+    private lateinit var textDisp: TextView
 
     override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?): View? {
         retView = inflater.inflate(R.layout.fragment_map, container, false)
 
+        hallBtn1 = retView.findViewById(R.id.imageButton_hall1_map)
+        textDisp = retView.findViewById(R.id.textView_display_text_map)
+        hallBtn1.setOnClickListener {
+            textDisp.text = "Showing events in hall 1"
+        }
+
         return retView
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
     }
 }
